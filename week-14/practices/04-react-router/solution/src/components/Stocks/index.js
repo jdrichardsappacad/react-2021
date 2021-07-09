cf3059c45565e77f9796afdd84138ebddd99a789
jdrichardsappacad <jrichards@appacademy.io>
2021-07-09 16:44:59 -0400

    add solution

--- week-14/practices/04-react-router/solution/src/components/Stocks/index.js
+++ week-14/practices/04-react-router/solution/src/components/Stocks/index.js
@@ -1,7 +1,22 @@
+import { useHistory, Redirect } from 'react-router-dom';
+
 function Stocks() {
+  const history = useHistory();
+  const loggedIn = true;
+
+  if (!loggedIn) return <Redirect to='/not-logged-in' />;
+
+  const handleClick = () => {
+    window.alert('Sending info to the DB!');
+
+    history.push('/');
+  };
+
   return (
-    <div className="comp orange">
+    <div className='comp orange'>
       <h1>Stocks Component</h1>
+
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
