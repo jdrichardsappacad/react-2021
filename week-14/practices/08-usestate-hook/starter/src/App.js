3ef77cbc52b0d38a92746231cae36865ddd1ac0b
jdrichardsappacad <jrichards@appacademy.io>
2021-07-28 13:23:23 -0400

    jd-usestate

---
+++ week-14/practices/08-usestate-hook/starter/src/App.js
@@ -0,0 +1,13 @@
+import UseState from './components/UseState';
+// import PreviousState from './components/PreviousState';
+
+function App() {
+  return (
+    <>
+      <UseState />
+      {/* <PreviousState /> */}
+    </>
+  );
+}
+
+export default App;
