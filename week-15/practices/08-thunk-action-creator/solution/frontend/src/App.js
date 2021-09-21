78051337621ebeaacb8dd3fd81666ab3f59caac9
Peter Mai <pm123633@gmail.com>
2021-09-21 18:22:33 -0500

    Set up frontend file structure

--- week-15/practices/08-thunk-action-creator/solution/frontend/src/App.js
+++ week-15/practices/08-thunk-action-creator/solution/frontend/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import ArticleList from './components/ArticleList';
+import ArticleInput from './components/ArticleInput';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ArticleList />
+      <ArticleInput />
     </div>
   );
 }
--- week-15/practices/08-thunk-action-creator/solution/frontend/src/index.js
+++ week-15/practices/08-thunk-action-creator/solution/frontend/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
+import configureStore from './store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as articleActions from './store/articleReducer';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+  window.articleActions = articleActions;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
