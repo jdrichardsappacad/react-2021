0081a2a97c93ba4f58957ad37c743cfb6bb12437
Peter Mai <pm123633@gmail.com>
2021-09-22 12:57:31 -0500

    Finish frontend
    * Create store
    * Create components

---
+++ week-15/practices/08-thunk-action-creator/solution/frontend/src/components/ArticleList/index.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+
+import SingleArticle from "../SingleArticle";
+import ArticleDetail from "../ArticleDetail";
+import { fetchArticles } from "../../store/articleReducer";
+
+const ArticleList = () => {
+  const dispatch = useDispatch();
+
+  const articles = useSelector((state) => state.articleState.entries);
+
+  useEffect(() => {
+    dispatch(fetchArticles());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>ArticleList</h1>
+      <ol>
+        {articles.map(({ id, title }) => (
+          <ArticleDetail key={id} id={id} title={title} />
+        ))}
+      </ol>
+
+      <Switch>
+        <Route path="/article/:id">
+          <SingleArticle articles={articles} />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default ArticleList;
