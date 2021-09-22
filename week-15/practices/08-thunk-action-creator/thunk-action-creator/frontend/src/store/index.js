5bbcc51f168899a19231dedcffbe51dd4698016e
Peter Mai <pm123633@gmail.com>
2021-09-22 17:55:49 -0500

    Remove comments concerning thunk middleware

--- week-15/practices/08-thunk-action-creator/thunk-action-creator/frontend/src/store/index.js
+++ week-15/practices/08-thunk-action-creator/thunk-action-creator/frontend/src/store/index.js
@@ -1,7 +1,5 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-//!!START SILENT
 import thunk from 'redux-thunk';
-//!!END
 
 import articleReducer from './articleReducer';
 import fruitReducer from './fruitReducer';
@@ -17,19 +15,9 @@ if (process.env.NODE_ENV !== 'production') {
   const logger = require('redux-logger').default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true }) || compose;
-  //!!START SILENT
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-  //!!END
-  //!!ADD
-  // enhancer = composeEnhancers(applyMiddleware(logger));
-  //!!END_ADD
 } else {
-  //!!START SILENT
   enhancer = applyMiddleware(thunk);
-  //!!END
-  //!!ADD
-  // enhancer = applyMiddleware();
-  //!!END_ADD
 }
 
 const configureStore = (preloadedState) => {
