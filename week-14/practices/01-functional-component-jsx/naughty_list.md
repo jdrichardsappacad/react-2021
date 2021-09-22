d3c0ec4163dfce4ef7a921961d4c1c834aaa9c5a
James Richards <jd-app@AppAcademyAir.home>
2021-09-22 14:39:05 -0400

    convert solution file

---
+++ week-14/practices/01-functional-component-jsx/naughty_list.md
@@ -0,0 +1,2 @@
+components/
+App.css
--- week-14/practices/01-functional-component-jsx/solution/src/App.js
+++ week-14/practices/01-functional-component-jsx/solution/src/App.js
@@ -1,10 +1,15 @@
+//!!START SILENT
 import Showcase from './components/Showcase';
 import './App.css';
+//!!END
 
 function App() {
   return (
     <div className='background'>
+      <h1>App Component</h1>
+      {/*!!START SILENT */}
       <Showcase />
+      {/*!!END */}
     </div>
   );
 }
