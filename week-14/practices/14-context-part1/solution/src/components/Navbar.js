2bea516a08138e922adc29c5d925fa6690562be7
James Richards <jd-app@AppAcademyAir.home>
2021-11-13 16:10:49 -0500

    fix Navbar components

--- week-14/practices/14-context-part1/solution/src/components/Navbar.js
+++ week-14/practices/14-context-part1/solution/src/components/Navbar.js
@@ -1,22 +1,14 @@
-import { useContext } from 'react';
-import { HoroscopeContext } from '../context/HoroscopeContext';
-import horoscopesObj from '../data/horoscopes';
+import horoscopeObj from '../data/horoscopes';
 
 const Navbar = () => {
-  const {setCurrentSign} = useContext(HoroscopeContext)
-  const horoscopes = Object.keys(horoscopesObj);
-
-  console.log('Navbar component');
-
+  const horoscopes = Object.keys(horoscopeObj);
   return (
     <nav>
       {horoscopes.map(sign => (
-        <span key={sign} onClick={() => setCurrentSign(sign)}>
-          {sign}
-        </span>
+        <span key={sign}>{sign}</span>
       ))}
     </nav>
-  )
+  );
 };
 
 export default Navbar;
