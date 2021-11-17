2b1c0897e1fb35ae8a77f8475eade3cc5a191fb3
Bill Adams <vikingbill@gmail.com>
2021-11-17 13:18:58 -0500

    progress

--- week-14/practices/08-usestate-hook/starter/src/components/UseState/index.js
+++ week-14/practices/08-usestate-hook/starter/src/components/UseState/index.js
@@ -1,7 +1,15 @@
+import "./UseState.css"
+import {useState} from 'react'
+
 const UseState = () => {
+  // console.log(useState('light'))
+  const [theme, setTheme] = useState('light');
+
   return (
-    <div>
+    <div className={theme}>
       <h1>UseState Component</h1>
+      <button onClick={()=>setTheme('dark')}>Dark</button>
+      <button onClick={()=>setTheme('lighjt')}>Light</button>
     </div>
   );
 };
