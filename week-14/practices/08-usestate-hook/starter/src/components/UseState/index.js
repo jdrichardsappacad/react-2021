45e14558f9763b4154bca6fc01c37f519b4f6715
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-11-17 10:34:32 -0800

    finished

--- week-14/practices/08-usestate-hook/starter/src/components/UseState/index.js
+++ week-14/practices/08-usestate-hook/starter/src/components/UseState/index.js
@@ -4,12 +4,17 @@ import {useState} from 'react'
 const UseState = () => {
   // console.log(useState('light'))
   const [theme, setTheme] = useState('light');
+  const [count, setCount] = useState(0);
 
   return (
-    <div className={theme}>
+    <div className={`${theme} container`}>
       <h1>UseState Component</h1>
-      <button onClick={()=>setTheme('dark')}>Dark</button>
-      <button onClick={()=>setTheme('lighjt')}>Light</button>
+      <button onClick={()=>setTheme(theme === 'light' ? 'dark' : 'light')}>{theme === 'light' ? 'dark' : 'light'}</button>
+      <button onClick={()=> setCount((prevState) => prevState + 1)}> Increment </button>
+      <h1> {count} </h1>
+      {/* <button onClick={()=>setTheme('light')}>Light</button> */}
+      <button onClick={()=> setCount((prevState) => prevState - 1)}> Decrement </button>
+
     </div>
   );
 };
