9a6d0ca96528295f2186cfc44ce6ddaba6def0ce
Ryan <rdmaloney3@gmail.com>
2021-11-18 18:29:40 -0500

    typos, wording, missing CSS file, fixed table warning

--- week-14/practices/03-creating-lists/starter/src/BaseStats.js
+++ week-14/practices/03-creating-lists/starter/src/BaseStats.js
@@ -8,23 +8,25 @@ const BaseStats = ({ clicker, stats: { hp, attack, defense, speed } }) => {
         Check Special Stats
       </button>
       <table>
-        <tr>
-          <td>Hit Points</td>
-          <td>{hp}</td>
-        </tr>
-        <tr>
-          <td>Attack</td>
-          <td>{attack}</td>
-        </tr>
-        <tr>
-          <td>Defense</td>
-          <td>{defense}</td>
-        </tr>
-        <tr>
-          <td>Speed</td>
-          <td>{speed}</td>
-        </tr>
-      </table>
+          <tbody>
+            <tr>
+              <td>Hit Points</td>
+              <td>{hp}</td>
+            </tr>
+            <tr>
+              <td>Attack</td>
+              <td>{attack}</td>
+            </tr>
+            <tr>
+              <td>Defense</td>
+              <td>{defense}</td>
+            </tr>
+            <tr>
+              <td>Speed</td>
+              <td>{speed}</td>
+            </tr>
+          </tbody>
+        </table>
     </div>
   );
 };
