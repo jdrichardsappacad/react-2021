4e45305db7d5b0b60553e655f6ec21c2a901f71b
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-11-19 09:40:13 -0800

    switch

---
+++ week-14/practices/12-react-forms/contact-us-form/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ContactUs from './components/ContactUs';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ContactUs />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
