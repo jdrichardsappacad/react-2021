627d0379d588259ab3c830af106cf9aa8e297a1a
jdrichardsappacad <jrichards@appacademy.io>
2021-12-02 18:57:17 -0500

    make changes

--- week-15/practices/03-react-redux-actions-reducer/starter/src/index.js
+++ week-15/practices/03-react-redux-actions-reducer/starter/src/index.js
@@ -9,7 +9,7 @@ const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
--- week-15/practices/03-react-redux-actions-reducer/starter/src/store/index.js
+++ week-15/practices/03-react-redux-actions-reducer/starter/src/store/index.js
@@ -1,26 +1,13 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 
-/* combineReducers turns all the reducer functions into one big reducer function
- */
-/* this is the most important part of this file. you will add your reducers here
-to work with your components. you are creating one big reducer */
+import fruitReducer from './fruitReducer';
 
-// create rootReducer here:
+const rootReducer = combineReducers({
+  fruitState: fruitReducer
+});
 
-/* enhancer allows you to alter the store and add functionality such as redux
- devtools, logger (similar to morgan) middleware */
+let enhancer;
 
-/* compose gives you the ability to add more than one enhancer to the store */
-/* env comes automatically in create-react-app */
-/* process.env.NODE_ENV has 3 settings:
-   1. running npm start makes process.env.NODE_ENV ='development`
-   2. running npm test makes process.env.NODE_ENV = 'test'
-   3. running npm run build makes process.env.NODE_ENV = 'production' which you
-      will use with heroku
-*/
-/* window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true }) along with the
-chrome extension for redux devtools will set up your Redux DevTools in the
-browser */
 if (process.env.NODE_ENV !== 'production') {
   const logger = require('redux-logger').default;
   const composeEnhancers =
@@ -28,14 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
   enhancer = composeEnhancers(applyMiddleware(logger));
 }
 
-/* createStore creates a store object literal {} */
-/* preloaded state, not important for now, mainly used for hydrating state from
-the server */
-/* enhancer see above */
-
-/*this is the variable you will use in your root index.js to give Redux store
-access to the full application */
-const configureStore = preloadedState => {
+const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
 
