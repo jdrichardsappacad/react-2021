38344ccd2a6ec36691843847ab0f1f44efa8508a
James Richards <jd-app@AppAcademyAir.home>
2022-01-18 16:45:55 -0500

    update solution file for lifecycle

--- week-15/practices/02-react-class-component-lifecycle-methods-short-practice/solution/src/App.js
+++ week-15/practices/02-react-class-component-lifecycle-methods-short-practice/solution/src/App.js
@@ -1,11 +1,18 @@
 import { Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar.js';
+//!!START SILENT
 import ClassTitan from './components/classComponents/ClassTitan.js';
+//!!END
+//!!ADD
 import FunctionTitan from './components/functionalComponents/FunctionTitan.js';
+//!!END_ADD
+//!!START SILENT
 // import ClassKuiper from './components/classComponents/ClassKuiper.js';
+//!!END
+//!!ADD
 import FunctionKuiper from './components/functionalComponents/FunctionKuiper.js';
-
+//!!END_ADD
 const App = () => {
   return (
     <div className='main-container'>
@@ -17,10 +24,19 @@ const App = () => {
           </div>
         </Route>
         <Route path='/titan'>
+          {/*!!START SILENT */}
           <ClassTitan />
+          {/*!!END*/}
+          {/*!!ADD */}
+          <FunctionTitan />
         </Route>
         <Route path='/kuiper'>
+          {/*!!START SILENT */}
+          <ClassKuiper />
+          {/*!!END*/}
+          {/*!!ADD */}
           <FunctionKuiper />
+          {/*!!END_ADD */}
         </Route>
       </Switch>
     </div>
