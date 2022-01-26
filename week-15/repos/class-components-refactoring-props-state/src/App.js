79fa39dc00cfeab45e2d432f6bbd9371feab0096
jdrichardsappacad <jrichards@appacademy.io>
2022-01-26 10:35:12 -0500

    not sure

--- week-15/repos/class-components-refactoring-props-state/src/App.js
+++ week-15/repos/class-components-refactoring-props-state/src/App.js
@@ -4,7 +4,6 @@ import FunctionComponent from './components/FunctionComponent';
 function App() {
   return (
     <>
-      <h1>App Component</h1>
       <ClassComponent title='Class Component' />
       <FunctionComponent title='Function Component' />
     </>
